3c7bba7745f6673d794d828eddfb8824eac63ebf
Adam Wright <32560955+adamdenverco@users.noreply.github.com>
2025-01-22 23:01:55 -0600

    codeacademy learning

---
+++ src/assets/scripts/codeacademy.js
@@ -0,0 +1,37 @@
+// let userName = "";
+
+// if (userName === "Jane") {
+//     console.log("Hello, Jane!");
+// } else {
+//     console.log("Hello!");
+// }
+
+// let userQuestion = "Is the sky blue?";
+
+// console.log("userQuestion", userQuestion);
+
+// let randomNumber = Math.floor(Math.random() * 8);
+// let num = randomNumber;
+
+// let eightBall = "";
+
+// if (num == 0) {
+//     eightBall = "It is certain";
+// } else if (num == 1) {
+//     eightBall = "It is decidedly so";
+// } else if (num == 2) {
+//     eightBall = "Reply hazy try again";
+// } else if (num == 3) {
+//     eightBall = "Cannot predict now";
+// } else if (num == 4) {
+//     eightBall = "Do not count on it";
+// } else if (num == 5) {
+//     eightBall = "My sources say no";
+// } else if (num == 6) {
+//     eightBall = "Outlook not so good";
+// } else if (num == 7) {
+//     eightBall = "Signs point to yes";
+// } else if (num == 8) {
+//     eightBall = "You may be right";
+// }
+// console.log("eightBall answer: ", eightBall);
