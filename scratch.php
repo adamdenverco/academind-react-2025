23655e8fcbd75bdfe12ea0f84e5bfbbc09880ee6
Adam Wright <32560955+adamdenverco@users.noreply.github.com>
2025-01-23 22:28:27 -0600

    more learning

--- scratch.php
+++ scratch.php
@@ -1,7 +1,54 @@
 <?php
 
-function calculateTip($total, $tip = 20) {
- return $total + ($total ($tip / 100));
-}
+// function calculateTip($total, $tip = 20) {
+//  return $total + ($total ($tip / 100));
+// }
 
-echo calculateTip(15, 15);
+// echo calculateTip(15, 15);
+
+
+// function calculateDistance($num1, $num2) {
+//   if ($num1 < 0 && $num2 < 0) {
+//     return abs($num1 - $num2);
+//   } else if ($num1 < 0 || $num2 < 0) {
+//     return abs($num1) + abs($num2);
+//   } else {
+//     return abs($num1 - $num2);
+//   }
+// }
+// echo "\n\n calculateDistance \n";
+// echo calculateDistance(-1, 4) . "\n";
+// echo calculateDistance(4, -1) . "\n";
+// echo calculateDistance(3, 7) . "\n";
+// echo calculateDistance(7, 3) . "\n";
+
+// function calculateTip($num) {
+//   return round($num + ($num * 0.18));
+// }
+
+// echo "\n\n calculateTipe \n";
+
+// echo calculateTip(100) . "\n";
+// echo calculateTip(35) . "\n";
+// echo calculateTip(88.77) . "\n";
+
+
+// echo rand(1, 2) . "\n"; // Prints either 1 or 2
+
+// echo rand(5, 10) . "\n"; // Prints a number between 5 and 10 (inclusive!)
+
+// echo rand(1, 100) . "\n"; // Prints a number between 1 and 100 (inclusive!)
+
+// echo "getrandmax: " . getrandmax() . "\n";
+// echo "rand: " . rand() . "\n";
+// echo "rand(1,52): " . rand(1,52) . "\n";
+
+
+// $a = 29;
+// $b = "You did it!";
+// $c = STR_PAD_BOTH;
+// $d = "*~*";
+
+// // Write your code below:
+
+// echo str_pad($b, $a, $d, $c);
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
 import React from "react";
+import logo from "./assets/images/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+const genRandomInt = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+};
+
+const titleWord = reactDescriptions[genRandomInt(2)];
 
 const Header = () => {
     return (
         <header className="font-RobotoCondensed text-center">
-            <img
-                className="inline"
-                src="src/assets/images/react-core-concepts.png"
-                alt="Stylized atom"
-            />
+            <img className="inline" src={logo} alt="Stylized atom" />
             <h1 className=" font-bold">React Essentials</h1>
             <p className="">
-                Fundamental React concepts you will need for almost any app you
+                {titleWord} React concepts you will need for almost any app you
                 are going to build!
             </p>
         </header>
     );
 };
 
+const CoreConcept = ({ concept }) => {
+    console.log("concept", concept);
+    return (
+        <li>
+            <img src={concept.image} alt={concept.title} />
+            <h3>{concept.title}</h3>
+            <p>{concept.description}</p>
+        </li>
+    );
+};
+
 const Main = () => {
+    const concepts = CORE_CONCEPTS;
+    console.log("concepts", concepts);
+
     return (
         <main className="font-RobotoCondensed">
             <h2 className="font-bold">Time to get started!</h2>
+            <section id="core-concepts">
+                <h2>Core Concepts</h2>
+                <ul>
+                    {concepts.map((concept, index) => {
+                        return <CoreConcept key={index} concept={concept} />;
+                    })}
+                </ul>
+            </section>
         </main>
     );
 };
